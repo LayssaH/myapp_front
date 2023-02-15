afb0c4a620ddcad08c6b2415ef81a86a365107db
layss <layssa.haouassi@edu.devinci.fr>
2023-02-15 21:22:07 +0100

    suppresion fichier inutiles

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,3 @@
-import * as api from '$lib/api';
-import { page_size } from '$lib/constants';
-import {redirect} from "@sveltejs/kit";
 
 /** @type {import('./$types').PageServerLoad} */
 export function load({ locals }) {
